fa548b2b4d0f0dc8eb54535290fcec6ba0a2a550
Timo Uhlmann <timouhlmann@gmail.com>
2015-03-05 12:14:37 -0500

    Refactored for CSP

---
+++ elements/playing-card.js
@@ -0,0 +1,37 @@
+
+    Polymer('playing-cards', {
+      /**
+       * The `noevent` event is not actually fired from here,
+       * we document it as an example of documenting events.
+       *
+       * @event noevent
+       */
+
+      /**
+       * The `attr` attribute does not yet have a purpose.
+       *
+       * @attribute attr
+       * @type string
+       */
+      attr: '',
+
+      ready: function() {
+      },
+
+      /**
+       * This function will be invoked every time `attr` gets changed.
+       */
+      attrChanged: function() {
+      },
+
+      /**
+       * The `task` method does [...]
+       *
+       * @method task
+       * @return {Object} Returns [...].
+       * @param {String} dummy Defines [...].
+       */
+      task: function(dummy) {
+      }
+    });
+  
